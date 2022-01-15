f786fa2f16e19046481477c44ed38d4cb4e260c7
Tamim <farman.arefin1@outlook.com>
2022-01-15 17:01:44 +0600

    CSES Ferris wheel

--- codechef_long.cpp
+++ codechef_long.cpp
@@ -157,23 +157,26 @@ int main()
     freopen("output.txt", "w", stdout);
     #endif
 
-   int n;
-   cin>>n;
+   int n,m;
+   cin>>n>>m;
    vector<int>v(n);
    for(int i=0; i<n; i++){
       cin>>v[i];
    }
 
    sort(v.begin(),v.end());
-   int count=0;
-
-   for(int i=0; i<n; i++){
-      if(v[i]==v[i+1])
-        continue;
-      else
-        count++;
+   int cnt=n;
+   int i=0,j=n-1;
+   while(i<j){
+       if(v[i]+v[j]<=m){
+         cnt--;
+         i++;
+         j--;
+       }else{
+         j--;
+       }
    }
-   cout<<count<<endl;
+   cout<<cnt<<endl;
     
     return 0;
 }
