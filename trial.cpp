5826eff661918b711d2f1127dab101e49c46245d
Tamim <farman.arefin1@outlook.com>
2024-05-19 20:37:45 +0600

    Added cses 1

---
+++ trial.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+
+#define int long long int
+#define ll long long
+using namespace std;
+int MOD = 1e9+7;
+
+#define pb push_back
+
+
+int sum(int a, int b){
+  return a+b;
+}
+
+
+
+
+int32_t main(){
+
+
+   #ifndef ONLINE_JUDGE 
+   freopen("input.txt", "r", stdin); 
+   freopen("output.txt", "w", stdout);
+   #endif
+
+    std::ios_base::sync_with_stdio(false);
+
+
+    //cses 1
+    // int n;
+    // cin>>n;
+
+    // while(n!=1){
+    //   cout<<n<<' ';
+    //   if(n&1){
+    //     n=3*n+1;
+    //   }else{
+    //     n>>=1;
+    //   }
+    // }
+    // cout<<1;
+
+    return 0;
+
+}
